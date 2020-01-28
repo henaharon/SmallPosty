7757f860113239400918e4165098e2434adcb3fe
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-01-28 23:09:26 +0200

    remove Sample

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
-* Sample React Native App
-* https://github.com/facebook/react-native
-*
-* @format
-* @flow
-*/
-
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
