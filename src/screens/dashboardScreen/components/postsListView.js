db75e2f7abd7ddd4f7e39cbea6cce945850a5a4e
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-01-31 14:37:16 +0200

    PostsListView

---
+++ src/screens/dashboardScreen/components/postsListView.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react';
+import { inject, observer } from 'mobx-react';
+import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import { PostCard } from './postCard/postCard';
+
+@inject('storeDashboardScreen')@observer
+export class PostsListView extends PureComponent {
+    render() {
+        const { postsArray } = this.props.storeDashboardScreen;
+        return (
+            <SafeAreaView style={styles.flexView}>
+                    <FlatList  data={postsArray}
+                            keyExtractor={(item, index) => item.post_id.toString()} 
+                            renderItem={(item) => <PostCard post={item} />} 
+                            />
+            </SafeAreaView>
+        )
+        }
+}
+
+const styles = StyleSheet.create({
+    flexView: {
+        flex: 1
+    }
+  });
