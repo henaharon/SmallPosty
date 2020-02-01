1652a81a23854984b913c0a24b0b1bfe4f496942
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-02-01 20:42:55 +0200

    rootStore replace mainStore

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {AppRegistry} from 'react-native';
 import { Provider } from 'mobx-react'
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';import App from './App';
 import {name as appName} from './app.json';
-import mainStore from './src/providers/mainStore';
+import rootStore from './src/providers/rootStore';
 
 const theme = {
   ...DefaultTheme,
@@ -21,7 +21,7 @@ const theme = {
 class Application extends Component {
     render() {
       return (
-        <Provider rootStore={mainStore}>
+        <Provider rootStore={rootStore}>
           <PaperProvider theme={theme}>
             <App />
           </PaperProvider>
