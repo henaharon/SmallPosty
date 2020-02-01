112f6704efd30c6c3742acfdb1fab5c9b17691b5
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-02-01 20:42:38 +0200

    navigation

--- App.js
+++ App.js
@@ -1,32 +1,76 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import Icon from 'react-native-vector-icons/Entypo'
 import { DashboardScreen } from './src/screens/dashboardScreen/DashboardScreen';
 import { ProfileScreen } from './src/screens/profileScreen/ProfileScreen';
 import { AuthLoadingScreen } from './src/screens/authScreen/AuthLoadingScreen';
 import { LoginScreen } from './src/screens/loginScreen/LoginScreen';
 import { RegisterScreen } from './src/screens/registerScreen/RegisterScreen';
+import { AddPostScreen } from './src/screens/addPostScreen/AddPostScreen';
+import { LoadingScreen } from './src/screens/loadingScreen.js/loadingScreen';
 
+const DashboardScreenStack = createStackNavigator({
+  DashboardStack: {
+    screen: DashboardScreen,
+    navigationOptions: {
+      headerShown: false,
+    }
+  },
+  AddPost: { 
+    screen: AddPostScreen, 
+    navigationOptions: {
+      headerShown: true,
+      headerTitle: 'ADD POST',
+      headerStyle: {
+        height: 40
+      },
+      headerTitleStyle: {
+        fontSize: 15
+      },
+      headerTitleAlign: 'center',
+  },
+  }
+});
+
+DashboardScreenStack.navigationOptions = ({ navigation }) => {
+  let tabBarVisible = true;
+  if (navigation.state.index > 0) {
+    tabBarVisible = false;
+  }
+
+  return {
+    tabBarVisible,
+  };
+};
 
 const MainNavigator = createMaterialBottomTabNavigator(
 {
     ProfileScreen: { 
       screen: ProfileScreen,
+      navigationOptions: {
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({tintColor}) => <Icon name="user" size={25} color={tintColor} /> 
+      }
   },
     DashboardScreen: { 
-      screen: DashboardScreen, 
+      screen: DashboardScreenStack,
+      navigationOptions: {
+        tabBarLabel: 'Dashboard',
+        tabBarIcon: ({tintColor}) => <Icon name="clipboard" size={25} color={tintColor} /> ,
+      } 
   },
+  headerLayoutPreset: 'center'
 },
 {
     initialRouteName: 'DashboardScreen',
-    activeColor: 'white',
-    inactiveColor: '#00adb5',
-    barStyle: { backgroundColor: '#00adb5' }
+    activeColor: '#00adb5',
+    inactiveColor: 'black',
+    barStyle: { backgroundColor: '#fdffff' },
+    shifting: true,
 },
-{
-headerLayoutPreset: 'center'
-}
 )
 
 const AuthStack = createStackNavigator(
@@ -49,6 +93,12 @@ const AuthStack = createStackNavigator(
             },
             headerTitleAlign: 'center'
         },
+        },
+        Loading : {
+          screen: LoadingScreen,
+          navigationOptions: {
+            headerShown: false,
+          }
         }
     });
 
